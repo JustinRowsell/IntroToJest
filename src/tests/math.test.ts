1560637c3a66e18488d21ae79a11c4c6ec629c97
JustinRowsell <Rowsell64@gmail.com>
2019-05-12 15:02:48 -0600

    trying a couple different  things that dont work

---
+++ src/tests/math.test.ts
@@ -0,0 +1,4 @@
+import * as math from '../utils/math';
+it('One plus two equals three', () => {
+  expect(math.sum(1, 2)).toBe(3);
+});
---
+++ src/utils/math.ts
@@ -0,0 +1,16 @@
+export function sum(x: number, y: number): number {
+  return x + y;
+}
+
+export function subtract(x: number, y: number): number {
+  return x - y;
+}
+
+export function multiply(x: number, y: number): number {
+  return x * y;
+}
+
+export function divide(x: number, y: number): number {
+  // do a unit test that checks if y is 0
+  return x / y;
+}
